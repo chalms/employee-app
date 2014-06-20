b6ff16689e39edeab5fb2a874d84a7451fb9a3b2
Jaclyn Foss <andrew.n.chalmers@gmail.com>
2014-06-20 10:01:37 -0400

    Users Create and show and index

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,17 +1,23 @@
 class Api::UsersController < ApiController
 
+  skip_before_filter :api_session_token_authenticate!, only: [:create]
   def index
-  	if user.is_admin? 
-  		 respond_with User.all
-  	else 
-  		return _not_authorized 
-  	end 
+    return _not_authorized unless current_user.is_admin?
   end
-
+  
   def show
     user = User.find_by_id(params[:id])
+
     return _not_found unless user
     return _not_authorized unless (current_user.is_manager || current_user.is_admin || (current_user == user))
-    respond_with user
+    respond_with json: user
+  end
+
+  def create 
+    puts params
+    @user = User.create!(params[:user]) 
+    if (@user) 
+      return render Session.create!(params[:user])
+    end 
   end
 end
